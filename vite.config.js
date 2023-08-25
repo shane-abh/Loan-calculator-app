83002f903023f7a207891f741e852f446c301737
Shane <shaneabh777@gmail.com>
2023-08-25 17:51:05 -0400

    corrected base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./Loan-calculator-app",
+  base: "./LoanCalculatorApp",
   plugins: [react()],
 })
